9cd409c73587a018bcfe631f2c11b90a5590f6f2
Song da young <116068642+butcreep@users.noreply.github.com>
2024-04-21 23:32:35 +0900

    관리자 페이지 초기 설정
    - header 생성
    - 로그인 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
 import React from "react";
-import { DatePicker, Button } from "antd";
+import Login from "./pages/login";
 
 const App = () => {
-  return (
-    <div>
-      <div>
-        <h2 className="text-blue-500 text-xl font-bold text-center">
-          Hello, React
-        </h2>
-        <p className="text-lg font-medium">Hello, Typescript!</p>
-      </div>
-      <DatePicker />
-      <Button type="primary">Primary Button</Button>
-    </div>
-  );
+  return <Login />;
 };
 
 export default App;
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div class="m-auto max-w-screen-sm flex justify-center items-center">
+      <p>simple law</p>
+    </div>
+  );
+};
+
+export default login;
