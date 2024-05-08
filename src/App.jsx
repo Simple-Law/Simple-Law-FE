c0e623bb49b5495984c2cfb823b3a38a892b331c
Song da young <116068642+butcreep@users.noreply.github.com>
2024-05-09 00:22:19 +0900

    경로 재수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Admin from "new/pages/Admin";
-import SignUp from "new/pages/SignUp";
-import Home from "new/pages/Home";
-import Login from "new/pages/Login";
+import Admin from "pages/Admin";
+import SignUp from "pages/SignUp";
+import Home from "pages/Home";
+import Login from "pages/Login";
 
 function App() {
   return (
