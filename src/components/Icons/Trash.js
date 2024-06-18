72ddceb9bda91154e98111b518dd965ea9457a3e
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-06-18 16:03:29 +0900

    fix: viewBox 추가

--- src/components/Icons/Trash.js
+++ src/components/Icons/Trash.js
@@ -1,14 +1,8 @@
-const SvgTrash = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
-    fill="none"
-    {...props}
-  >
+const SvgTrash = props => (
+  <svg xmlns='http://www.w3.org/2000/svg' width={24} height={24} fill='none' {...props} viewBox='0 0 24 24'>
     <path
-      fill="#25282B"
-      d="M16.5 6.6H21v1.8h-1.8v11.7a.9.9 0 0 1-.9.9H5.7a.9.9 0 0 1-.9-.9V8.4H3V6.6h4.5V3.9a.9.9 0 0 1 .9-.9h7.2a.9.9 0 0 1 .9.9zm.9 1.8H6.6v10.8h10.8zm-8.1 2.7h1.8v5.4H9.3zm3.6 0h1.8v5.4h-1.8zM9.3 4.8v1.8h5.4V4.8z"
+      fill='#25282B'
+      d='M16.5 6.6H21v1.8h-1.8v11.7a.9.9 0 0 1-.9.9H5.7a.9.9 0 0 1-.9-.9V8.4H3V6.6h4.5V3.9a.9.9 0 0 1 .9-.9h7.2a.9.9 0 0 1 .9.9zm.9 1.8H6.6v10.8h10.8zm-8.1 2.7h1.8v5.4H9.3zm3.6 0h1.8v5.4h-1.8zM9.3 4.8v1.8h5.4V4.8z'
     />
   </svg>
 );
