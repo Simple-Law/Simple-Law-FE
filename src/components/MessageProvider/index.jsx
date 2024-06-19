5a2e274a8c0f99fe88ff9bf456633bbe65474857
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-06-19 11:22:26 +0900

    docs: MessageProvider 컴포넌트에 주석 추가

--- src/components/MessageProvider/index.jsx
+++ src/components/MessageProvider/index.jsx
@@ -6,6 +6,11 @@ const MessageApiContext = createContext(null);
 
 export const useMessageApi = () => useContext(MessageApiContext);
 
+/**
+ * MessageProvider 컴포넌트
+ * 하위 컴포넌트들에게 메시지 API를 제공
+ * Ant Design의 message 컴포넌트를 사용하여 메시지 전역 관리.
+ */
 const MessageProvider = () => {
   const [messageApi, contextHolder] = message.useMessage();
 
@@ -17,4 +22,4 @@ const MessageProvider = () => {
   );
 };
 
-export default MessageProvider; // MessageProvider를 default로 내보냄
+export default MessageProvider;
