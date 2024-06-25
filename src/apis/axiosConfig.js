5aa4ba83bec86765fb104ef843c7b9b089737510
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-06-25 14:54:30 +0900

    refactor: store를 사용해 토큰 가져오도록 수정

--- src/apis/axiosConfig.js
+++ src/apis/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { store } from "../redux/store";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
@@ -7,7 +8,9 @@ const axiosInstance = axios.create({
 });
 // Axios 요청 인터셉터를 사용하여 토큰을 자동으로 헤더에 추가
 axiosInstance.interceptors.request.use(config => {
-  const token = localStorage.getItem("accessToken");
+  const state = store.getState();
+  const token = state.auth.tokens.accessToken;
+  // const token = localStorage.getItem("accessToken");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
