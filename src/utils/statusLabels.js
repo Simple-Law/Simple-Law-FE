a36e894dbe9b0d57b86cc5624afdaa844e52575d
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-07-03 11:14:23 +0900

    feat: statusLabels 객체 모듈화

---
+++ src/utils/statusLabels.js
@@ -0,0 +1,22 @@
+const statusLabels = {
+  lawyer: {
+    preparing: "컨택 요청 중",
+    pending: "해결 진행 중",
+    completed: "해결 완료",
+    refuse: "신청거절",
+  },
+  quest: {
+    preparing: "의뢰 요청 중",
+    pending: "해결 진행 중",
+    completed: "해결 완료",
+    refuse: "신청거절",
+  },
+  guest: {
+    preparing: "비로그인 요청 중",
+    pending: "비로그인 해결 진행 중",
+    completed: "비로그인 해결 완료",
+    refuse: "비로그인 신청거절",
+  },
+};
+
+export default statusLabels;
