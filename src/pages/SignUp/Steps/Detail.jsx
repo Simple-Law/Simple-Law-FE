6ec8424fa3dc12c88bf042fb34a4fc44b377a2f9
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-07-15 12:00:17 +0900

    refactor: JoinForm,Detail 컴포넌트에 yup 적용

--- src/pages/SignUp/Steps/Detail.jsx
+++ src/pages/SignUp/Steps/Detail.jsx
@@ -1,5 +1,7 @@
 import { useState, useRef } from "react";
 import PropTypes from "prop-types";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, Controller } from "react-hook-form";
 import LoginForm from "components/layout/AuthFormLayout";
 import { Button, Form, Input } from "antd";
 import { useMessageApi } from "components/messaging/MessageProvider";
@@ -7,6 +9,7 @@ import { uploadFile } from "apis/commonAPI";
 import { ReactComponent as UploadFile } from "assets/images/icons/Upload.svg";
 import SvgTrash from "components/Icons/Trash";
 import { AiOutlinePaperClip } from "react-icons/ai";
+import { detailFormSchema } from "utils/validations";
 
 // 사무실 번호 지역번호 하이픈 추가
 const formatPhoneNumber = value => {
@@ -44,7 +47,16 @@ const formatPhoneNumber = value => {
 };
 
 const Detail = ({ handleData, nextStep }) => {
-  const [form] = Form.useForm();
+  const {
+    control,
+    handleSubmit: onSubmit,
+    formState: { errors, isValid },
+    setValue,
+    trigger,
+  } = useForm({
+    resolver: yupResolver(detailFormSchema),
+    mode: "onBlur",
+  });
   const [fileList, setFileList] = useState([]);
   const [loading, setLoading] = useState(false);
   const messageApi = useMessageApi();
@@ -59,6 +71,8 @@ const Detail = ({ handleData, nextStep }) => {
       const response = await uploadFile(formData);
       const fileUploadId = response?.data?.payload[0]?.fileUploadId; // 서버 응답에서 fileUploadId 추출
       messageApi.success(`${file.name} 파일이 성공적으로 업로드되었습니다.`);
+      setValue("fileUploadId", fileUploadId); // 파일 업로드 ID 설정
+      trigger("fileUploadId"); // 폼 상태 업데이트
       return fileUploadId;
     } catch (error) {
       messageApi.error(`${file.name} 파일 업로드에 실패했습니다.`);
@@ -81,23 +95,45 @@ const Detail = ({ handleData, nextStep }) => {
     if (beforeUpload(file)) {
       const newFileList = [file]; // 최신 파일만 유지
       setFileList(newFileList);
+      uploadToServer(file); // 파일 업로드 및 ID 설정
     }
   };
 
   const handleRemove = () => {
     setFileList([]);
+    setValue("fileUploadId", ""); // 파일 업로드 ID 초기화
+    trigger("fileUploadId"); // 폼 상태 업데이트
   };
 
   const handlePhoneNumberChange = e => {
     const { value } = e.target;
     const formattedPhoneNumber = formatPhoneNumber(value);
-    form.setFieldsValue({ companyPhone: formattedPhoneNumber });
+    setValue("companyPhone", formattedPhoneNumber);
+    trigger("companyPhone"); // 폼 상태 업데이트
   };
 
   const handleNumberChange = (fieldName, e) => {
     const { value } = e.target;
     const numericValue = value.replace(/[^0-9]/g, "");
-    form.setFieldsValue({ [fieldName]: numericValue });
+    setValue(fieldName, numericValue);
+    trigger(fieldName); // 폼 상태 업데이트
+  };
+
+  const beforeUpload = file => {
+    const isValidType = ["image/png", "image/jpeg", "image/jpg", "image/gif"].includes(file.type);
+    const isLt10M = file.size / 1024 / 1024 < 10;
+
+    if (!isValidType) {
+      messageApi.error("png, jpg, jpeg, gif 파일만 업로드할 수 있습니다.");
+      return false;
+    }
+
+    if (!isLt10M) {
+      messageApi.error("파일 크기는 10MB 이하만 가능합니다.");
+      return false;
+    }
+
+    return true;
   };
 
   const handleSubmit = async () => {
@@ -121,168 +157,120 @@ const Detail = ({ handleData, nextStep }) => {
     }
   };
 
-  const beforeUpload = file => {
-    const isValidType = ["image/png", "image/jpeg", "image/jpg", "image/gif"].includes(file.type);
-    const isLt10M = file.size / 1024 / 1024 < 10;
-
-    if (!isValidType) {
-      messageApi.error("png, jpg, jpeg, gif 파일만 업로드할 수 있습니다.");
-      return false;
-    }
-
-    if (!isLt10M) {
-      messageApi.error("파일 크기는 10MB 이하만 가능합니다.");
-      return false;
-    }
-
-    return true;
-  };
-
   return (
     <LoginForm title='변호사 회원가입'>
-      <Form className='flex gap-[20px] flex-col' form={form} name='validateOnly' autoComplete='off' onFinish={onFinish}>
+      <Form className='flex gap-[20px] flex-col' onFinish={onSubmit(onFinish)}>
         <div className='flex gap-2 flex-col'>
           <p className='font-medium text-base'>소속</p>
-          <Form.Item
-            name='companyName'
-            rules={[
-              {
-                required: true,
-                message: "소속을 입력해 주세요.",
-              },
-              {
-                whitespace: true,
-              },
-            ]}
-          >
-            <Input placeholder='소속(사무소,회사)' />
+          <Form.Item validateStatus={errors.companyName ? "error" : ""} help={errors.companyName?.message}>
+            <Controller
+              name='companyName'
+              control={control}
+              render={({ field }) => <Input placeholder='소속(사무소,회사)' {...field} />}
+            />
           </Form.Item>
-          <Form.Item
-            name='companyPhone'
-            rules={[
-              {
-                required: true,
-                message: "소속 전화번호를 입력해 주세요.",
-              },
-              {
-                whitespace: true,
-              },
-            ]}
-          >
-            <Input placeholder='소속 전화번호(‘-’ 제외하고 입력)' onChange={handlePhoneNumberChange} maxLength='13' />
+          <Form.Item validateStatus={errors.companyPhone ? "error" : ""} help={errors.companyPhone?.message}>
+            <Controller
+              name='companyPhone'
+              control={control}
+              render={({ field }) => (
+                <Input placeholder='소속 전화번호(‘-’ 제외하고 입력)' {...field} onChange={handlePhoneNumberChange} />
+              )}
+            />
           </Form.Item>
         </div>
         <div className='flex gap-2 flex-col'>
           <p className='font-medium text-base'>출신 시험</p>
-          <Form.Item
-            name='barExam'
-            rules={[
-              {
-                required: true,
-                message: "출신 시험을 입력해 주세요.",
-              },
-              {
-                whitespace: true,
-              },
-            ]}
-          >
-            <Input placeholder='출신 시험' />
+          <Form.Item validateStatus={errors.barExam ? "error" : ""} help={errors.barExam?.message}>
+            <Controller
+              name='barExam'
+              control={control}
+              render={({ field }) => <Input placeholder='출신 시험' {...field} />}
+            />
           </Form.Item>
-          <Form.Item
-            name='barExamCount'
-            rules={[
-              {
-                required: true,
-                message: "시험 횟수를 입력해 주세요.",
-              },
-              {
-                whitespace: true,
-              },
-            ]}
-          >
-            <Input placeholder='시험 횟수' onChange={e => handleNumberChange("barExamCount", e)} />
+          <Form.Item validateStatus={errors.barExamCount ? "error" : ""} help={errors.barExamCount?.message}>
+            <Controller
+              name='barExamCount'
+              control={control}
+              render={({ field }) => (
+                <Input placeholder='시험 횟수' {...field} onChange={e => handleNumberChange("barExamCount", e)} />
+              )}
+            />
           </Form.Item>
-
-          <Form.Item
-            name='yearOfPassing'
-            rules={[
-              {
-                required: true,
-                message: "변호사 자격 획득연도를 입력해 주세요.",
-              },
-              {
-                whitespace: true,
-              },
-            ]}
-          >
-            <Input
-              placeholder='변호사 자격 획득연도'
-              maxLength={4}
-              onChange={e => handleNumberChange("yearOfPassing", e)}
+          <Form.Item validateStatus={errors.yearOfPassing ? "error" : ""} help={errors.yearOfPassing?.message}>
+            <Controller
+              name='yearOfPassing'
+              control={control}
+              render={({ field }) => (
+                <Input
+                  placeholder='변호사 자격 획득연도'
+                  maxLength={4}
+                  {...field}
+                  onChange={e => handleNumberChange("yearOfPassing", e)}
+                />
+              )}
             />
           </Form.Item>
         </div>
-        <Form.Item
-          name='fileUploadId'
-          rules={[
-            {
-              required: true,
-              message: "파일을 업로드해 주세요.",
-            },
-          ]}
-        >
-          <div
-            ref={dropAreaRef}
-            onDragOver={e => e.preventDefault()}
-            onDrop={handleDrop}
-            style={{ position: "relative" }}
-          >
-            <p className='font-medium text-base flex items-center'>
-              변호사 신분증 사진 업로드
-              <input
-                type='file'
-                id='file-upload'
-                style={{ display: "none" }}
-                onChange={e => {
-                  const file = e.target.files[0];
-                  if (file) {
-                    handleFileChange(file);
-                  }
-                }}
-              />
-              <label
-                htmlFor='file-upload'
-                className='bg-slate-400 rounded text-white text-xs font-medium leading-none px-2 py-1 ml-[10px]'
-                style={{ cursor: "pointer" }}
+        <Form.Item validateStatus={errors.fileUploadId ? "error" : ""} help={errors.fileUploadId?.message}>
+          <Controller
+            name='fileUploadId'
+            control={control}
+            render={({ field }) => (
+              <div
+                ref={dropAreaRef}
+                onDragOver={e => e.preventDefault()}
+                onDrop={handleDrop}
+                style={{ position: "relative" }}
               >
-                내 PC
-              </label>
-            </p>
-            {fileList.length === 0 ? (
-              <div className='h-[180px] text-center rounded-md border border-dashed border-gray-200 bg-gray-50 mt-2 flex flex-col items-center justify-center'>
-                <UploadFile className='mx-auto' />
-                <p className='ant-upload-hint text-Btn-Text-Disabled text-sm font-normal mb-[10px]'>
-                  최대 10mb 이하 png, jpg, jpeg, gif
+                <p className='font-medium text-base flex items-center'>
+                  변호사 신분증 사진 업로드
+                  <input
+                    type='file'
+                    id='file-upload'
+                    style={{ display: "none" }}
+                    onChange={e => {
+                      const file = e.target.files[0];
+                      if (file) {
+                        handleFileChange(file);
+                      }
+                    }}
+                  />
+                  <label
+                    htmlFor='file-upload'
+                    className='bg-slate-400 rounded text-white text-xs font-medium leading-none px-2 py-1 ml-[10px]'
+                    style={{ cursor: "pointer" }}
+                  >
+                    내 PC
+                  </label>
                 </p>
-              </div>
-            ) : (
-              <div className='border border-dashed border-gray-200 rounded-md p-2 bg-gray-50 mt-2'>
-                {fileList.map((file, index) => (
-                  <div key={index} className='flex justify-between items-center p-2 mb-2 '>
-                    <div className='flex items-center'>
-                      <AiOutlinePaperClip className='mr-2' />
-                      <span className='text-sm text-gray-700'>{file.name}</span>
-                    </div>
-                    <Button type='link' onClick={handleRemove} className='p-0 '>
-                      <SvgTrash width='15px' height='15px' />
-                    </Button>
+                {fileList.length === 0 ? (
+                  <div className='h-[180px] text-center rounded-md border border-dashed border-gray-200 bg-gray-50 mt-2 flex flex-col items-center justify-center'>
+                    <UploadFile className='mx-auto' />
+                    <p className='ant-upload-hint text-Btn-Text-Disabled text-sm font-normal mb-[10px]'>
+                      최대 10mb 이하 png, jpg, jpeg, gif
+                    </p>
                   </div>
-                ))}
+                ) : (
+                  <div className='border border-dashed border-gray-200 rounded-md p-2 bg-gray-50 mt-2'>
+                    {fileList.map((file, index) => (
+                      <div key={index} className='flex justify-between items-center p-2 mb-2 '>
+                        <div className='flex items-center'>
+                          <AiOutlinePaperClip className='mr-2' />
+                          <span className='text-sm text-gray-700'>{file.name}</span>
+                        </div>
+                        <Button type='link' onClick={handleRemove} className='p-0 '>
+                          <SvgTrash width='15px' height='15px' />
+                        </Button>
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
             )}
-          </div>
+          />
         </Form.Item>
-        <Button type='primary' htmlType='submit' block disabled={loading}>
+        <Button type='primary' htmlType='submit' block disabled={!isValid || loading}>
           다음
         </Button>
       </Form>
