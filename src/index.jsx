ca475c1a4cd28646304a34ba6507a14673129296
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-08-07 15:18:45 +0900

    refactor: browser router 위치 index.js로 수정

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
 import LoadingSpinner from "components/layout/LoadingSpinner";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,7 +15,9 @@ root.render(
     <PersistGate loading={null} persistor={persistor}>
       <ConfigProvider theme={theme}>
         <LoadingSpinner />
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ConfigProvider>
     </PersistGate>
   </Provider>,
