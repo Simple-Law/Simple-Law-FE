2caf9cf24f31482ab532b8e6ce4aa8990a48752e
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-08-22 18:02:37 +0900

    refactor: 모달 컴포넌트에 footer 추가

--- src/components/modal/ConfirmModal.jsx
+++ src/components/modal/ConfirmModal.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 import { Modal } from "antd";
 
-const ConfirmModal = ({ title, visible, onOk, onCancel, children }) => (
-  <Modal title={title} open={visible} onOk={onOk} onCancel={onCancel}>
+const ConfirmModal = ({ title, visible, onOk, onCancel, children, footer }) => (
+  <Modal title={title} open={visible} onOk={onOk} onCancel={onCancel} footer={footer}>
     {children}
   </Modal>
 );
@@ -13,6 +13,7 @@ ConfirmModal.propTypes = {
   onOk: PropTypes.func.isRequired,
   onCancel: PropTypes.func.isRequired,
   children: PropTypes.node,
+  footer: PropTypes.node,
 };
 
 export default ConfirmModal;
