d58d8f6e0bb2fef5cdd64a20910bb82f73cf864c
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-09-06 17:18:55 +0900

    refactor: 폼에서 필요한 필드만 유효성 검사를 하도록 수정
    - 아이디, 비밀번호 필드 추가

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -48,7 +48,7 @@ export const validationSchema = yup.object().shape({
     .matches(/^010-\d{4}-\d{4}$/, "올바른 전화번호 양식이 아닙니다.")
     .required("전화번호는 필수로 입력해야 합니다."),
 
-  verificationCode: yup.string().required("인증번호를 입력하세요."),
+  verificationCode: yup.string().required("인증번호를 입력하세요!").length(4, "인증번호는 4자리여야 합니다."),
 });
 
 export const detailFormSchema = yup.object().shape({
@@ -68,4 +68,23 @@ export const detailFormSchema = yup.object().shape({
     .required("변호사 자격 획득연도를 입력해 주세요.")
     .matches(/^\d{4}$/, "올바른 연도 형식이 아닙니다."),
   fileUploadId: yup.string().required("파일을 업로드해 주세요."),
+});
+export const findUserIdSchema = yup.object().shape({
+  name: yup.string().required("이름은 필수로 입력해야 합니다."),
+  email: yup.string().email("올바른 이메일 양식이 아닙니다.").required("이메일은 필수로 입력해야 합니다!"),
+});
+export const findUserPWSchema = yup.object().shape({
+  id: yup.string().required("아이디는 필수로 입력해야 합니다."),
+  name: yup.string().required("이름은 필수로 입력해야 합니다."),
+  phoneNumber: yup
+    .string()
+    .transform(value => {
+      // 숫자만 남기고 포맷팅
+      const cleanedValue = value.replace(/\D/g, "");
+      return cleanedValue.length === 11 ? cleanedValue.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3") : value;
+    })
+    .matches(/^010-\d{4}-\d{4}$/, "올바른 전화번호 양식이 아닙니다.")
+    .required("전화번호는 필수로 입력해야 합니다."),
+
+  verificationCode: yup.string().required("인증번호를 입력하세요!").length(4, "인증번호는 4자리여야 합니다."),
 });
