6d6f58ff0e256d110dd3bc19c8ad060a8ecfa742
devkmee <devkmee@gmail.com>
2024-10-17 01:09:25 +0900

    Feat: 관리자 의뢰함 목록 퍼블리싱 및  API 테스트

--- src/pages/Admin/Request/RequestList.jsx
+++ src/pages/Admin/Request/RequestList.jsx
@@ -1,62 +1,105 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useCommonContext } from "contexts/CommonContext";
 import { AdminBoard, AdminPageWrap, TableEmptyDiv } from "components/styled/StyledComponents";
 import SvgSearch from "components/Icons/Search";
 import { PageSearch } from "pages/Request/MyRequestList";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Table } from "antd";
-import { SkeletonLoading } from "components/layout/LoadingSpinner";
 import { statusLabels } from "utils/statusLabels";
+import { FaRegStar, FaStar } from "react-icons/fa";
+import { StatusTag } from "components/tags/StatusTag";
+import { getRequestList } from "apis/adminRequestAPI";
+import { useMessageApi } from "components/messaging/MessageProvider";
+import { hideSkeletonLoading, showSkeletonLoading } from "../../../redux/actions/loadingAction";
+import { SkeletonLoading } from "components/layout/LoadingSpinner";
 
 const ManageAdminList = () => {
-  const { paginationConfig } = useCommonContext();
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-
-  const [pageTitle, setPageTitle] = useState("");
-
-  const loading = useSelector(state => state.loading.SkeletonLoading);
-
   const columns = [
     {
-      width: 32,
+      key: "isImportant",
+      dataIndex: "isImportant",
+      width: 48,
+      // onCell: record => ({
+      //   onClick: e => handleToggleImportant(record.caseKey, e),
+      // }),
+      render: (_, record) => (
+        <div
+          style={{
+            fontSize: "18px",
+            display: "flex",
+            justifyContent: "center",
+            color: record.isImportant ? "gold" : "#CDD8E2",
+          }}
+        >
+          {record.isImportant ? <FaStar /> : <FaRegStar />}
+        </div>
+      ),
     },
     {
       title: "상태",
-      key: "a",
-      render: <span></span>,
+      key: "status",
+      dataIndex: "status",
+      render: status => <StatusTag status={status} userType={userType} />,
+      width: 150,
+      className: "status-column",
     },
     {
       title: "분야",
-      key: "b",
+      key: "mainCaseCategoryName",
       render: <span></span>,
     },
     {
       title: "세부분야",
-      key: "c",
+      key: "subCaseCategoryName",
       render: <span></span>,
     },
     {
       title: "제목",
-      key: "d",
+      key: "title",
       render: <span></span>,
     },
     {
       title: "의뢰 요청시간",
-      key: "e",
+      key: "requestedAt",
       render: <span></span>,
     },
     {
       title: "제한시간",
-      key: "f",
+      key: "notifiedAt",
       render: <span></span>,
     },
   ];
 
+  const user = useSelector(state => state.auth.user) || {};
+  const userType = user.type;
+
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const statusArray = params.get("displayStatus") ? params.get("displayStatus").split(",") : [];
+
+  const [pageTitle, setPageTitle] = useState("");
+  const { paginationConfig } = useCommonContext();
+
+  const initialSearchParams = {
+    keyword: "",
+    startRequestedAt: "",
+    endRequestedAt: "",
+    displayStatus: statusArray,
+    mainCategoryKey: "",
+    subCategoryKey: "",
+    pageNumber: 1,
+    pageSize: paginationConfig.pageSize,
+  };
+  const [searchParams, setSearchParams] = useState(initialSearchParams);
+  const [data, setData] = useState([]);
+
+  const messageApi = useMessageApi();
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.loading.SkeletonLoading);
+
   useLayoutEffect(() => {
     const pageTitles = statusLabels.ADMIN;
-    const statusArray = params.get("displayStatus") ? params.get("displayStatus").split(",") : [];
 
     // 페이지 타이틀 설정
     if (statusArray.length > 0) {
@@ -68,6 +111,32 @@ const ManageAdminList = () => {
       setPageTitle("전체 의뢰함");
     }
   }, [location]);
+
+  useEffect(() => {
+    selectRequestList();
+  }, []);
+
+  /**
+   * 관리자 의뢰함 목록 조회
+   */
+  const selectRequestList = async () => {
+    try {
+      const apiParams = {
+        ...searchParams,
+        displayStatus: searchParams.displayStatus.join(","),
+      };
+
+      dispatch(showSkeletonLoading());
+      const response = await getRequestList(apiParams);
+      if (response.status === 200 && response.data.status === "success") {
+        setData(response.data.data.payload);
+      }
+    } catch (err) {
+      messageApi.error(err?.response?.data?.message);
+    } finally {
+      dispatch(hideSkeletonLoading());
+    }
+  };
 
   return (
     <AdminPageWrap>
@@ -90,7 +159,7 @@ const ManageAdminList = () => {
               className='border-t-2'
               rowKey={data => data.key}
               columns={columns}
-              // dataSource={data}
+              dataSource={data}
               pagination={paginationConfig}
               locale={{
                 emptyText: (
